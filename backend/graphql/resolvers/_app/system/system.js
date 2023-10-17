c4acf4ba0b53a2c139d7fb6d59adb9252dbc0a9f
DevOS <john@togoenvogue.com>
2023-10-17 11:31:36 +0000

    Online setup OK 3

--- backend/graphql/resolvers/_app/system/system.js
+++ backend/graphql/resolvers/_app/system/system.js
@@ -412,7 +412,7 @@ const appResolver = {
 
     // remove duplicates from the urls
     const uniqueFileUrls = [...new Set(fileUrls)];
-    console.log(`uniqueFileUrls: ${uniqueFileUrls}`);
+    //console.log(`uniqueFileUrls: ${uniqueFileUrls}`);
     // download the images
     for (let inxo = 0; inxo < uniqueFileUrls.length; inxo++) {
       const url = uniqueFileUrls[inxo];
